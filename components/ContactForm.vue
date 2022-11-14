83181830e13edc5ebb959857521e1d05496a73c9
torerik123 <62653782+torerik123@users.noreply.github.com>
2022-11-14 14:39:20 +0100

    FEAT: Form validation

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -6,13 +6,50 @@
 			</v-col>
 		</v-row>
 
-		<v-row dense no-gutters class="px-15">
+		<p class="text-center" v-if="messageSent">Message sent!</p>
+
+		<v-row dense no-gutters class="">
 			<v-col cols="12">
-				<v-form class="mx-auto mb-2" style="width: 60%;">
-					<v-text-field solo label="Name" hide-details class="mb-4"></v-text-field>
-					<v-text-field solo label="Email" hide-details class="mb-4"></v-text-field>
-					<v-textarea solo label="Message" hide-details class="mb-4"></v-textarea>
-					<v-btn type="submit" color="primary">Submit</v-btn>
+				<v-form 
+					ref="form"
+					class="mx-auto mb-2" 
+					:style="$vuetify.breakpoint.smAndDown ? 'width: 100%;' : 'width: 70%;' "
+				>
+					<v-text-field 
+						label="Name"
+						v-model="name" 
+						required 
+						solo 
+						hide-details="auto" 
+						class="mb-4"
+						:rules="[rules.required]"
+					>
+					</v-text-field>
+					<v-text-field 
+						label="Email" 
+						v-model="email" 
+						required 
+						solo 
+						hide-details="auto" 
+						class="mb-4"
+						:rules="[rules.email]"
+					></v-text-field>
+					<v-textarea 
+						label="Message"
+						v-model="message"
+						required 
+						solo 
+						hide-details="auto" 
+						class="mb-4"
+						:rules="[rules.required]"
+					>
+					</v-textarea>
+					<v-btn
+						large 
+						@click="validate"
+						color="primary">
+						Submit
+					</v-btn>
 				</v-form>
 			</v-col>
 		</v-row>
@@ -22,5 +59,44 @@
 <script>
 export default {
 	name: "ContactForm",
+
+	data() {
+		return {
+			rules: {
+				required: value => !!value || 'Field can not be empty.',
+				email: value => {
+					const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+					return pattern.test(value) || 'Invalid e-mail.'
+				},
+			},
+			valid: true,
+			name: "",
+			email: "",
+			message: "",
+			messageSent: false,
+		}
+	},
+
+	methods: {
+		validate() {
+			let valid = this.$refs.form.validate()
+
+			if (valid) {
+				console.log("--TODO")
+				console.log("--Send form")
+				console.log(this.name)
+				console.log(this.email)
+				console.log(this.message)
+				
+				this.name = ""
+				this.email = ""
+				this.message = ""
+				
+				this.messageSent = true
+				// action="https://formspree.io/f/xpzoydbv"
+				  // 		method="POST"
+			}
+		}
+	}
 }
 </script>
--- pages/TODO.vue
+++ pages/TODO.vue
@@ -1,8 +1,8 @@
 <template>
 	<v-list>
+		<v-list-item>Form -> Formspree.io</v-list-item>
 		<v-list-item>Separate stuff into components</v-list-item>
 		<v-list-item>Make everything response</v-list-item>
-		<v-list-item>Fix header navigation + smooth scroll</v-list-item>
 		<v-list-item>Optimize images</v-list-item>
 		<v-list-item>Animations</v-list-item>
 	</v-list>
