ba6108a3d160ea37979d70d6c44bdcb7b6bb3034
torerik123 <62653782+torerik123@users.noreply.github.com>
2022-11-16 19:52:03 +0100

    FIX: Header navigation when not on home page

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -4,22 +4,22 @@
 			<v-hover v-slot="{ hover }">
 				
 				<v-img class="" :src="logo" max-height="100" max-width="100" contain
-					:style="hover ? 'cursor: pointer;' : ''" @click="scrollTo('home')"></v-img>
+					:style="hover ? 'cursor: pointer;' : ''" @click="scrollOrRedirect('home')"></v-img>
 			</v-hover>
 
 			<v-spacer></v-spacer>
 			<v-toolbar-title class="" v-if="$vuetify.breakpoint.smAndUp">
-				<v-btn text tile @click="scrollTo('about')">
+				<v-btn text tile @click="scrollOrRedirect('about')">
 					About
 				</v-btn>
 			</v-toolbar-title>
 			<v-toolbar-title class="" v-if="$vuetify.breakpoint.smAndUp">
-				<v-btn text tile @click="scrollTo('gallery')">
+				<v-btn text tile @click="scrollOrRedirect('gallery')">
 					Gallery
 				</v-btn>
 			</v-toolbar-title>
 			<v-toolbar-title class="" v-if="$vuetify.breakpoint.smAndUp">
-				<v-btn text tile @click="scrollTo('contact')">
+				<v-btn text tile @click="scrollOrRedirect('contact')">
 					Contact
 				</v-btn>
 			</v-toolbar-title>
@@ -31,23 +31,23 @@
 				<v-list-item>
 					<v-list-item-content>
 						<v-hover v-slot="{ hover }">
-							<v-img :style="hover ? 'cursor: pointer;' : ''" @click="scrollTo('home')" class="mx-auto"
+							<v-img :style="hover ? 'cursor: pointer;' : ''" @click="scrollOrRedirect('home')" class="mx-auto"
 								:src="logo" max-height="100" max-width="100" contain></v-img>
 						</v-hover>
 					</v-list-item-content>
 				</v-list-item>
 
-				<v-list-item @click="scrollTo('about')">
+				<v-list-item @click="scrollOrRedirect('about')">
 					<v-list-item-content class="v-btn">
 						About
 					</v-list-item-content>
 				</v-list-item>
-				<v-list-item @click="scrollTo('gallery')">
+				<v-list-item @click="scrollOrRedirect('gallery')">
 					<v-list-item-content class="v-btn">
 						Gallery
 					</v-list-item-content>
 				</v-list-item>
-				<v-list-item @click="scrollTo('contact')">
+				<v-list-item @click="scrollOrRedirect('contact')">
 					<v-list-item-content class="v-btn">
 						Contact
 					</v-list-item-content>
@@ -76,18 +76,27 @@ export default {
 	},
 
 	methods: {
-		scrollTo(section) {
-			if (section == "home") {
-				this.$vuetify.goTo(0)
-			} else {
-				this.$vuetify.goTo("#" + section)
-			}
+		async scrollOrRedirect(section) {
+			if (this.isHomePage) {
+				const location = section == "home" ? 0 : `#${section}`
 
+				this.$vuetify.goTo(location)
+			} else {
+				section = section == "home" ? "/" : "/" + section
+ 				this.$router.push(section)
+			} 
+				
 			if (this.drawer) {
 				this.drawer = false
 			}
 		},
-	}
+	},
+
+	computed: {
+		isHomePage() {
+			return this.$route.path === "/"
+		},
+	},
 }
 </script>
 
