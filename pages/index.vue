2d35e117cc63e6b80d9275b7a754ddbf51d21323
torerik123 <62653782+torerik123@users.noreply.github.com>
2022-11-16 19:13:34 +0100

    merge conflict

--- pages/index.vue
+++ pages/index.vue
@@ -9,49 +9,6 @@
 	<!-- Images  -->
 	<v-sheet id="gallery" height="auto" class="">
 		<v-row dense no-gutters class="justify-space-between">
-<<<<<<< Updated upstream
-			<v-col cols="6" class="">
-				<v-img
-					class="mx-auto"
-					src="/assets/taakemaleri.jpg"
-					max-width="300"
-					height="auto"
-					contain
-				>
-				</v-img>
-			</v-col>
-			<v-col cols="6" class="">
-				<v-img
-					class="mx-auto"
-					src="/assets/kraake.jpg"
-					max-width="300"
-					contain
-					height="auto"
-				>
-				</v-img>
-			</v-col>
-		</v-row>
-		<v-row dense no-gutters class="pt-10 justify-space-between">
-			<v-col cols="6" class="">
-				<v-img
-					class="mx-auto"
-					src="/assets/blameis.jpg"
-					max-width="300"
-					height="auto"
-					contain
-				>
-				</v-img>
-			</v-col>
-			<v-col cols="6" class="">
-				<v-img
-					class="mx-auto"
-					src="/assets/skisse.jpg"
-					max-width="300"
-					contain
-					height="auto"
-				>
-				</v-img>
-=======
 			<v-col
 				v-for="image in images"
 				:key="image.src"
@@ -63,7 +20,6 @@
 					@open="handleOpen"
 					@viewImg="handlePreview" 
 				></ImageSingle>
->>>>>>> Stashed changes
 			</v-col>
 		</v-row>
 	</v-sheet>
@@ -77,12 +33,10 @@
 import ContactForm from "../components/ContactForm.vue";
 import HeroSection from "../components/HeroSection.vue";
 import SectionHeader from "../components/SectionHeader.vue";
+import ImageSingle from "../components/ImageSingle.vue";
 
 export default {
 	name: "Index",
-<<<<<<< Updated upstream
-	components: {ContactForm, HeroSection, SectionHeader}
-=======
 	components: { ContactForm, HeroSection, SectionHeader, ImageSingle },
 
 	data() {
@@ -90,32 +44,6 @@ export default {
 			dialog: false,
 			dialogImg: "",
 			images: this.$store.getters.images,
-			// images: [
-			// 	{
-			// 		title: "Tåke",
-			// 		src: require('~/assets/taakemaleri.jpg'),
-			// 		size: "100 x 100",
-			// 		sold: true,
-			// 	},
-			// 	{
-			// 		title: "Kråke",
-			// 		src: require('~/assets/kraake.jpg'),
-			// 		size: "100 x 100",
-			// 		sold: true,
-			// 	},
-			// 	{
-			// 		title: "Blåmeis",
-			// 		src: require('~/assets/blameis.jpg'),
-			// 		size: "100 x 100",
-			// 		sold: false,
-			// 	},
-			// 	{
-			// 		title: "Skisse",
-			// 		src: require('~/assets/skisse.jpg'),
-			// 		size: "100 x 100",
-			// 		sold: false, 
-			// 	}
-			// ],
 		}	
 	},
 
@@ -129,6 +57,5 @@ export default {
 			console.log("todo")
 		},
 	}
->>>>>>> Stashed changes
 }
 </script>
