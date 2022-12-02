90dc0252882c69e6e121502e020c6929f3007cdb
torerik123 <62653782+torerik123@users.noreply.github.com>
2022-12-02 16:41:19 +0100

    FIX: Import font in config

--- pages/TODO.vue
+++ pages/TODO.vue
@@ -1,12 +1,22 @@
 <template>
 	<v-list>
-		<v-list-item>/about - fix header</v-list-item>
+		<v-list-item>Font not being used?</v-list-item>
+		<v-list-item>Notify DATOCMS after successful build</v-list-item>
+		<!-- # Successful build notification example
+		curl -n -X POST https://webhooks.datocms.com/yqztWHMNJ1/deploy-results \
+		-H 'Content-Type: application/json' \
+		-d '{ "status": "success" }'
+
+		# Failed build notification example
+		curl -n -X POST https://webhooks.datocms.com/yqztWHMNJ1/deploy-results \
+		-H 'Content-Type: application/json' \
+		-d '{ "status": "error" }' -->
+		
 		<v-list-item>Redirect /admin to datocms</v-list-item>
-		<v-list-item>Set favicon</v-list-item>
+		<v-list-item>Use SSR</v-list-item>
 		<v-list-item>Use optimized images -> webp from datoCMS</v-list-item>
 		<v-list-item>Animations</v-list-item>
 		<v-list-item>Loading spinners -> lazy loading osv</v-list-item>
-		<v-list-item>Find Better font</v-list-item>
 		<v-list-item>Check everything response</v-list-item>
 		<v-list-item> 
 				--> improve lighthouse score
